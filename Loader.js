f63c70703461bd75675c83f104a348307bab9815
PAWAN SETHI <66323571+pawansethi2111@users.noreply.github.com>
2023-05-24 22:38:38 +0530

    Add files via upload

---
+++ Loader.js
@@ -0,0 +1,16 @@
+import { CssBaseline } from "@material-ui/core";
+import React from "react";
+
+import HashLoader from "react-spinner/HashLoader";
+
+function Loader(){
+    let [loading, setLoading] = usestate(true);
+
+    let [color, setColor] = useState('#ffffff');
+
+    const override = css 
+    
+    display : block;
+    margin: 0 auto;
+
+}
